5564157ee9efee83315c6f1068e8403118f0d52c
Miguel Andres <miguelandres254656@gmail.com>
2025-06-04 02:00:53 -0400

    Revert "aa"
    This reverts commit a95ebf9d5a8f1b1a1d2b485b9f153e4ea8b8fff3.

--- vite.config.js
+++ vite.config.js
@@ -21,10 +21,7 @@ export default defineConfig({
         }
     },
     server: {
-        hmr: {
-            host: '127.0.0.1',
-        },
-        host: '127.0.0.1',
+        host: '0.0.0.0',
         port: 5173,
     }
 });
