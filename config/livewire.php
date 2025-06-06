a02ec30c8c72d4db5e44ac02001a9efa2df0acd6
Miguel Andres <miguelandres254656@gmail.com>
2025-06-05 21:52:01 -0400

    Revert "a"
    This reverts commit 66ced6337edade4a6ed06e693d99c86cc229f326.

--- config/livewire.php
+++ config/livewire.php
@@ -38,7 +38,7 @@ return [
     |
     */
 
-    'layout' => 'layouts.app',
+    'layout' => 'components.layouts.app',
 
     /*
     |---------------------------------------------------------------------------
@@ -64,17 +64,17 @@ return [
     */
 
     'temporary_file_upload' => [
-        'disk' => 'public',        // Changed from null to 'public'
-        'rules' => ['required', 'file', 'max:12288'],
-        'directory' => 'livewire-tmp',
-        'middleware' => null,
-        'preview_mimes' => [   
+        'disk' => null,        // Example: 'local', 's3'              | Default: 'default'
+        'rules' => null,       // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
+        'directory' => null,   // Example: 'tmp'                      | Default: 'livewire-tmp'
+        'middleware' => null,  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
+        'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs...
             'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
             'mov', 'avi', 'wmv', 'mp3', 'm4a',
             'jpg', 'jpeg', 'mpga', 'webp', 'wma',
         ],
-        'max_upload_time' => 5,
-        'cleanup' => true,
+        'max_upload_time' => 5, // Max duration (in minutes) before an upload is invalidated...
+        'cleanup' => true, // Should cleanup temporary uploads older than 24 hrs...
     ],
 
     /*
