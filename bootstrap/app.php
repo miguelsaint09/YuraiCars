118b50f8f75a4f6cc5561ba042f4fb746de9c9c7
Miguel Andres <miguelandres254656@gmail.com>
2025-06-05 22:37:23 -0400

    a

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\RedirectIfAdmin;
+use App\Http\Middleware\ForceHttps;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -15,6 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->appendToGroup('web', [
             RedirectIfAdmin::class,
         ]);
+        
+        // Force HTTPS in production
+        $middleware->append(ForceHttps::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
